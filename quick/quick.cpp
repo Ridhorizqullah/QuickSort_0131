7f4d131fc98485d4226680ba91c6cae504503508
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-03-29 10:02:42 +0700

    menampilkan

--- quick/quick.cpp
+++ quick/quick.cpp
@@ -78,4 +78,19 @@ void q_short(int low, int high)
 	q_short(j + 1, high);
 }
 
-void display()
+void display() {
+
+	cout << "\n----------------" << endl;
+	cout << "sorted array" << endl;
+	cout << "------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nnumber of comparasions: " << cmp_count << endl;
+	cout << "number of data movements: " << mov_count << endl;
+}
+
+int main()
